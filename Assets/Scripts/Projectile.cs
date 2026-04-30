002e48ed178b70225d41d3f83c0fa77af537eaa0
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2026-04-30 12:13:51 +0100

    Added Flash when using Echo

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,16 +1,24 @@
+using System;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private AudioClip hitSound;
+    [SerializeField] private float lifespan;
+    private float _timer;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (hitSound != null)
-        {
-            AudioSource.PlayClipAtPoint(hitSound, transform.position); 
-        }
+        if (hitSound != null) AudioSource.PlayClipAtPoint(hitSound, transform.position); 
 
         Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= lifespan)
+            Destroy(gameObject);
     }
 }
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -4,11 +4,14 @@ using UnityEngine.Rendering.Universal;
 
 public class Shooting : MonoBehaviour
 {
-    [SerializeField] private GameObject projectilprefab;
+    [SerializeField] private GameObject projectilPrefab;
     [SerializeField] private float speed;
     [SerializeField] private Vector2 offset = new Vector2(1f, 0f);
     [SerializeField] private string inputName;
     [SerializeField] private AudioClip sound;
+    [Header("Flash Setts")]
+    [SerializeField] private Animator lightAnim;
+    [SerializeField] private string lightAnimName = "Flash";
 
     private Rigidbody2D _rb;
     private InputAction _input;
@@ -30,7 +33,7 @@ public class Shooting : MonoBehaviour
     {
         Vector2 spawn = (Vector2)transform.position + offset;
 
-        GameObject projectile = Instantiate(projectilprefab, spawn, transform.rotation);
+        GameObject projectile = Instantiate(projectilPrefab, spawn, transform.rotation);
 
         _rb = projectile.GetComponent<Rigidbody2D>();
         if (_rb != null)
@@ -38,6 +41,7 @@ public class Shooting : MonoBehaviour
             _rb.linearVelocity = transform.right * speed;
         }
 
-        AudioSource.PlayClipAtPoint(sound, transform.position);
+        if (sound) AudioSource.PlayClipAtPoint(sound, transform.position);
+        if (lightAnim) lightAnim.SetTrigger(lightAnimName);
     }
 }
